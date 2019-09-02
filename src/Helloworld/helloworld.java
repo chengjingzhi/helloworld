4743bfa1755332c0d27fbb9f1c63dc58d02419b5
chengjingzhi <acer@LAPTOP-CT29F0O2>
2019-09-02 15:59:26 +0800

    test

--- src/Helloworld/helloworld.java
+++ src/Helloworld/helloworld.java
@@ -3,5 +3,6 @@ package Helloworld;
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("hello-world,my name is BruceLn!");
+		System.out.println("Welcome to ShaanXi!");
 	}
 }
