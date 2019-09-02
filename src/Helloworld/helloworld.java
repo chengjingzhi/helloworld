5b9cd0b2b8c4c78d7079dd0ec51f594710ded288
chengjingzhi <acer@LAPTOP-CT29F0O2>
2019-09-02 16:10:18 +0800

    test

--- src/Helloworld/helloworld.java
+++ src/Helloworld/helloworld.java
@@ -3,6 +3,8 @@ package Helloworld;
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("hello-world,my name is BruceLn!");
+		System.out.println("Welcome to China!");
 		System.out.println("Welcome to ShaanXi!");
+
 	}
 }
